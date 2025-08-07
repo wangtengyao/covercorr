a864d4fb747c171873a2538572396cf5ae5f6184
Wang59 <wangtengyao@gmail.com>
2025-08-07 10:18:38 +0100

    add dataset

---
+++ R_package/covercorr/R/CD8T.R
@@ -0,0 +1,17 @@
+#' Dataset: CD8+T cell gene expression data
+#' @description
+#' The CD8T dataset provides the gene expression data of fetal CD8+T cells
+#' obtained in single-cell RNA sequencing experiment. 
+#' @usage CD8T
+#' @format Data are provided in
+#' raw sequencing counts as a data frame with 9369 rows (cells) and 1000 
+#' columns (genes). 
+#' @source The original data were part of the published data by Suo and 
+#' colleagues in Science (2022). Here we use only cells that were annotated 
+#' to be CD8+T and the top 1000 highly variable genes. 
+#' @references
+#' Suo, Chenqu, Emma Dann, Issac Goh, Laura Jardine, Vitalii Kleshchevnikov, 
+#' Jong-Eun Park, Rachel A. Botting et al. 
+#' "Mapping the developing human immune system across organs." 
+#' Science 376, no. 6597 (2022): eabo0510.
+"CD8T"
